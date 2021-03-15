8e0e9efaaff9d5cc71f30a019bc33d8766bb5860
Laxman-koirala <nirlax10000bc@gmail.com>
2021-03-15 16:29:17 +0200

    Test Design done

---
+++ TestActionLibrary.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+import names
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+from random import randint
+from selenium.webdriver.support.wait import WebDriverWait
